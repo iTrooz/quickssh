d2d35c0cb650bc8876ab11af62e19cb1dc70f12f
iTrooz <hey@itrooz.fr>
2023-12-15 00:25:28 +0100

    move init logger code in init_logger()

--- src/cli.rs
+++ src/cli.rs
@@ -19,7 +19,10 @@ fn init_server_key() -> anyhow::Result<KeyPair> {
     let existing_prv_key_path = xdg.find_config_file("private.key");
     if let Some(existing_prv_key_path) = existing_prv_key_path {
         let keypair = russh_keys::load_secret_key(&existing_prv_key_path, None)?;
-        log::debug!("Loaded private key from {}", existing_prv_key_path.display());
+        log::debug!(
+            "Loaded private key from {}",
+            existing_prv_key_path.display()
+        );
         Ok(keypair)
     } else {
         let keypair = russh_keys::key::KeyPair::generate_ed25519().unwrap();
@@ -34,17 +37,20 @@ fn init_server_key() -> anyhow::Result<KeyPair> {
     }
 }
 
-pub async fn run(cmd: Command) -> anyhow::Result<()> {
-    // init logger
+fn init_logger(verbose: bool) {
     let mut tmp = env_logger::builder();
     let mut log_builder = &mut tmp;
-    if cmd.verbose {
+    if verbose {
         log_builder = log_builder.filter_level(log::LevelFilter::Debug);
         log::debug!("Debug logging enabled");
     } else {
         log_builder = log_builder.filter_level(log::LevelFilter::Info);
     }
     log_builder.init();
+}
+
+pub async fn run(cmd: Command) -> anyhow::Result<()> {
+    init_logger(cmd.verbose);
 
     let keypair = init_server_key()?;
 

51fd42df152acd364ada07967c321e2593863a87
iTrooz <hey@itrooz.fr>
2023-12-17 01:10:27 +0100

    Automatically detect and set allowed auth methods

--- src/ssh/init.rs
+++ src/ssh/init.rs
@@ -22,8 +22,18 @@ pub struct ServerOptions {
 }
 
 pub async fn start_ssh_server(options: ServerOptions, keypair: KeyPair) -> anyhow::Result<()> {
+    let mut methods_list: Vec<MethodSet> = vec![];
+    if options.password.is_some() {
+        methods_list.push(MethodSet::PASSWORD);
+    }
+    if !options.pubkeys.is_empty() {
+        methods_list.push(MethodSet::PUBLICKEY);
+    }
+
+    let methods = methods_list.into_iter().reduce(|a, b| a | b);
+
     let config = russh::server::Config {
-        methods: MethodSet::PASSWORD | MethodSet::PUBLICKEY,
+        methods: methods.expect("At least one authentication method should be available"),
         inactivity_timeout: Some(std::time::Duration::from_secs(60 * 60)),
         auth_rejection_time: std::time::Duration::from_secs(5),
         auth_rejection_time_initial: Some(std::time::Duration::from_secs(0)),
